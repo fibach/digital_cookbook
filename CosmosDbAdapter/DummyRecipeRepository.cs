bdc9b0c83d4f41d47c55ba935a9dddb41fd3ee25
FI_COOKBOOK\admin <fibach@grenke.de>
2021-09-16 14:28:15 +0200

    Implementing Dummy recipe repo

--- CosmosDbAdapter/DummyRecipeRepository.cs
+++ CosmosDbAdapter/DummyRecipeRepository.cs
@@ -4,10 +4,55 @@ using rest.Shared;
 namespace CosmosDbAdapter
 {
     internal class DummyRecipeRepository : IRecipeRepository
-    {
+    {        
         public IEnumerable<Recipe> GetAllRecipes()
         {
-            throw new NotImplementedException();
+            return new[]
+            {
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+                RecipeFactory,
+            };
         }
+
+        private static Recipe RecipeFactory => new Recipe
+        {
+            Id = Guid.NewGuid(),
+            Title = RandomString(20),
+            Instruction = RandomString(200),
+            Ingredients = new[]
+            {
+                IngredientFactory,
+                IngredientFactory
+            }
+        };
+
+        private static Ingredient IngredientFactory => new Ingredient
+        {
+            Amount= new Random().Next(100),
+            Unit = Unit.g,
+            Name = RandomString(20)
+        };
+
+        private static string RandomString(int length)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
     }
 }
