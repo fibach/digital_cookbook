fd714b5dbbefb1220532e5dd85bb22c047c8da37
FI_COOKBOOK\admin <fibach@grenke.de>
2021-09-16 14:16:17 +0200

    adding image to recipe

--- rest/Shared/Recipe.cs
+++ rest/Shared/Recipe.cs
@@ -4,10 +4,8 @@ namespace Labdays.DigitalCookbook.Rest.Shared
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-
         public string Instruction { get; set; }
-
         public IEnumerable<Ingredient> Ingredients { get; set; }
-        
+        public byte[] Image { get; set; }
     }
 }
