9173653612b99fa6e1ef7a6e5b8099741feba8fa
Dennis Seiffert <dseiffert@grenke.de>
2021-09-16 13:59:53 +0200

    add cookbook controller

---
+++ rest/Server/Controllers/CookbookController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using rest.Shared;
+
+namespace rest.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CookbookController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<CookbookController> _logger;
+
+        public CookbookController(ILogger<CookbookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
