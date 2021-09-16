c8fe9155126bdeb13b80deb3cc0c2423f8e04067
Dennis Seiffert <dseiffert@grenke.de>
2021-09-16 14:10:41 +0200

    simple domain model

---
+++ rest/Shared/Ingredient.cs
@@ -0,0 +1,10 @@
+namespace Labdays.DigitalCookbook.Rest.Shared
+{
+    public class Ingredient
+    {
+        public double Amount { get; set; }
+
+        public Unit Unit {get; set;}
+        
+    }
+}
---
+++ rest/Shared/Recipe.cs
@@ -0,0 +1,13 @@
+namespace Labdays.DigitalCookbook.Rest.Shared
+{    
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public string Instruction { get; set; }
+
+        public IEnumerable<Ingredient> Ingredients { get; set; }
+        
+    }
+}
