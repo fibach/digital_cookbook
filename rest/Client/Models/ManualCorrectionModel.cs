78d1578a95be4f1c1d81b6d864f9bb2fab8d3989
FI_COOKBOOK\admin <fibach@grenke.de>
2021-09-17 14:49:46 +0200

    adding some ui convenience

--- rest/Client/Models/ManualCorrectionModel.cs
+++ rest/Client/Models/ManualCorrectionModel.cs
@@ -6,5 +6,7 @@
         public string? Instruction { get; set; }
 
         public string? Title { get; set; }
+
+        public bool IsEmpty => Ingredients == null && Instruction == null && Title == null;
     }
 }
